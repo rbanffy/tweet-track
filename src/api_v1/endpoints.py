db071afb03d605c676f96dfb77f30f5ea46584ef
Ricardo Bánffy <rbanffy@gmail.com>
2020-01-04 13:57:42 +0000

    Add skeleton responses

--- src/api_v1/endpoints.py
+++ src/api_v1/endpoints.py
@@ -5,15 +5,46 @@ The API endpoints, /person, {service}/id/{id}, and
 {service}/screen_name/{screen_name}
 """
 
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
 import http
 
-endpoints = Blueprint(
-    'person', __name__,
-    template_folder='templates')
+from flask import Blueprint, abort, jsonify, render_template
+from jinja2 import TemplateNotFound
 
-@endpoints.route('/person/<person_id>')
+endpoints = Blueprint("person", __name__, template_folder="templates")
+
+
+@endpoints.route("/person/<person_id>/info.json", methods=["GET"])
 def person(person_id: int):
-    return 'A person'
+    "Get the perso data from the store and return it as JSON"
+
+    return jsonify(
+        {
+            "person_id": person_id,
+            "twitter_id": 1234,
+            "latest_twitter_screen_name": "abc",
+            "twitter_screen_names": {
+                "2018-01-01": "bbbn",
+                "2019-12-20": "abc",
+            },
+            "first_seen": "2018-01-01",
+            "last_seen": "2019-19-20",
+        }
+    )
     # abort(http.HTTPStatus.NOT_FOUND)
+
+
+@endpoints.route("/<service>/id/<user_id>/info.json", methods=["GET"])
+def id_on_service(service, user_id):
+    return jsonify(
+        {
+            "person_id": 1234,
+            "twitter_id": user_id,
+            "last_twitter_screen_name": "abc",
+            "twitter_screen_names": {
+                "2018-01-01": "bbbn",
+                "2019-12-20": "abc",
+            },
+            "first_seen": "2018-01-01",
+            "last_seen": "2019-19-20",
+        }
+    )
