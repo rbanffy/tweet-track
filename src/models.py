4bbfebe1af8066b119dd821d4d8a8495d8a387c3
Ricardo Bánffy <rbanffy@gmail.com>
2020-01-27 21:19:14 +0000

    Add dummy Entity class and tests

--- src/models.py
+++ src/models.py
@@ -3,3 +3,29 @@
 
 Models using the Google App Engine datastore APIs.
 """
+
+
+class Entity:
+    def __init__(self, name, description=""):
+        self.name = name
+        self.description = description
+        self.key = None
+
+    def save(self):
+        # TODO: Actually save
+        self.key = "TEYUItyu3y"
+        return self.key
+
+    @classmethod
+    def get_by_name(cls, name):
+        # TODO: Get something
+        e = Entity(name="Entity 1", description="Entity 1's description")
+        e.key = "TEYUItyu3y"
+        return e
+
+    @classmethod
+    def get_by_key(cls, key):
+        # TODO: Get something
+        e = Entity(name="Entity 1", description="Entity 1's description")
+        e.key = "TEYUItyu3y"
+        return e
--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -2,27 +2,30 @@
 
 import unittest
 
-import models
+from models import Entity
 
 
 class TestEntity(unittest.TestCase):
     "An Entity controls one or more accounts in one or mode service"
 
-    def test_create_entity(self):
-        e = Entity(
-            name="Entity 1",
-            description="Entity 1's description"
-        )
-        self.assertNull(e.key)
+    def setUp(self):
+        e = Entity(name="Entity 1", description="Entity 1's description")
+        self.assertIsNone(e.key)
         self.key = e.save()
-        self.assertNotNull(e.key)
-        self.assertEqual(e.name, "Entity 1")
-        self.assertEqual(e.description, "Entity 1's description")
+
+    def test_create_entity(self):
+        e = Entity(name="Entity 2", description="Entity 2's description")
+        self.assertIsNone(e.key)
+        e.save()
+        self.assertIsNotNone(e.key)
+        self.assertEqual(e.name, "Entity 2")
+        self.assertEqual(e.description, "Entity 2's description")
 
     def test_get_entity_by_name(self):
-        e = Entity.get_by_name('Entity 1')
+        e = Entity.get_by_name("Entity 1")
         self.assertEqual(e.description, "Entity 1's description")
 
-    def test_get_entity_by_id(self):
-        e = Entity.get_by_id(self.key)
+    def test_get_entity_by_key(self):
+        e = Entity.get_by_key(self.key)
+        self.assertEqual(e.name, "Entity 1")
         self.assertEqual(e.description, "Entity 1's description")
