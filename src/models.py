c1499333788992c1b0a6a7355bbd5c68c2c0af31
Ricardo Bánffy <rbanffy@gmail.com>
2020-01-28 21:07:51 +0000

    Flesh out the basics of the Account class.

--- src/models.py
+++ src/models.py
@@ -4,6 +4,8 @@
 Models using the Google Firestore in Native mode.
 """
 
+import datetime
+
 
 class Entity:
     def __init__(self, name, description=""):
@@ -37,8 +39,36 @@ class Entity:
 
 
 class Account:
-    def __init__(self, service_name, screen_name=None, service_id=None):
+    def __init__(self, service_name, service_id=None):
         self.service_name = service_name
-        self.screen_name = screen_name
+        # This is the id within a given service.
         self.service_id = service_id
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        # key is populated on save.
         self.key = None
+        # Screen names is a list of (timestamp, screen_name) across
+        # multiple updates of the record.
+        self.screen_names = []
+        # TODO: At the end of the __init__ mark this for updating by
+        # scheduling an update job for this service and id.
+
+    def save(self):
+        # TODO: Actually save
+        self.key = "TEYUItyu3y"
+        return self.key
+
+    @classmethod
+    def get_by_service_and_id(cls, service, service_id):
+        # TODO: Get something
+        ...
+
+    @classmethod
+    def get_by_key(cls, key):
+        # TODO: Get something
+        ...
+
+    def update(self):
+        "Calls the service APIs to update its data."
+        # TODO: Update the data.
+        ...
